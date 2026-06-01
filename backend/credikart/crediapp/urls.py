6987b65919942f8263dbc139b654b9084cd2cad9
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2026-06-01 17:07:12 +0530

    Add create_admin path to urls.py

--- backend/credikart/crediapp/urls.py
+++ backend/credikart/crediapp/urls.py
@@ -35,6 +35,6 @@ urlpatterns = [
     path("get_checkout/", get_checkout,name='get_checkout'),
     path("save_checkout/", save_checkout,name='save_checkout'),
     path("repayment_schedule/<int:order_id>/", repayment_schedule,name='repayment_schedule'),
-
+    path("create_admin/",create_admin,name='create_admin'),
 
 ]
