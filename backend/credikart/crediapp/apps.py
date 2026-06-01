78a67da8ca754564e9158d67b1c33d28c5e63759
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2026-06-01 22:44:42 +0530

    Implement admin user creation in CrediappConfig
    Add a ready method to create a default admin user if it doesn't exist.

--- backend/credikart/crediapp/apps.py
+++ backend/credikart/crediapp/apps.py
@@ -4,3 +4,16 @@ from django.apps import AppConfig
 class CrediappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'crediapp'
+
+    def ready(self):
+        from .models import User
+
+        try:
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    password="Admin123"
+                )
+                print("Admin created")
+        except:
+            pass
