6096a1189d47ae81401784ff1e0369fe1eb1968d
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2026-06-01 15:46:31 +0530

    Refactor database settings for flexibility
    Update database settings to use DATABASE_URL environment variable for production and fallback to SQLite for development.

--- backend/credikart/credikart/settings.py
+++ backend/credikart/credikart/settings.py
@@ -97,11 +97,19 @@ RAZORPAY_KEY_SECRET = "Vn72aLVlAAOW5AS4N16XCdRC"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
