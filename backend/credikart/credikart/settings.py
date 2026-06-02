25ba1a4b6ed028c1fa1d795e5c6e00b93a6f5a2c
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2026-06-02 16:31:50 +0530

    Update settings.py

--- backend/credikart/credikart/settings.py
+++ backend/credikart/credikart/settings.py
@@ -108,11 +108,15 @@ if DATABASE_URL:
     }
 else:
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'credikartdb3',
+        'USER': 'postgres',
+        'PASSWORD': 'Anjaly',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
