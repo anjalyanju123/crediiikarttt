9ef20b2ac6bedb07daca11e10b444cb2bc1b0861
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2026-06-04 14:51:05 +0530

    Update urls.py

--- backend/credikart/credikart/urls.py
+++ backend/credikart/credikart/urls.py
@@ -25,12 +25,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('crediapp.urls')),
     path("token/refresh/", TokenRefreshView.as_view()),
-    re_path(r"^.*$", TemplateView.as_view(
-        template_name="index.html"
-    )),
 ]
 
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += [
+    re_path(
+        r"^.*$",
+        TemplateView.as_view(template_name="index.html")
+    ),
+]
